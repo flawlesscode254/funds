d3543ed2728e6d79ab0691c4ea56313343254e3a
flawlesscode254 <denzme414@gmail.com>
2022-12-15 15:00:15 +0300

    Done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+require("dotenv").config()
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +20,7 @@ app.get("/", (req, res) => {
     })
 })
 
-const port = 3213
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log("App started");
