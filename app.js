de077b6ccd982d76293c238a3eef11a206b0a725
flawlesscode254 <denzme414@gmail.com>
2022-12-15 16:32:54 +0300

    Done

--- app.js
+++ app.js
@@ -24,30 +24,41 @@ app.post("/ussd", async (req, res) => {
 
   let response = "";
 
-  let brokendown = text.split("*");
-  let merchantCode = brokendown[0];
-  let amount = brokendown[1];
-
-  if (merchantCode !== "") {
-    response = `CON Checking for existing merchant`;
-    const existingMerchant = await merchantRecord.findOne({
-      merchantID: text,
-    });
-    if (existingMerchant) {
-      const newRecord = await new recordModel({
-        sessionId,
-        serviceCode,
-        phoneNumber,
-        text: amount,
-      });
-      response = `CON Saving request`;
-      await newRecord.save();
-      response = `END Completed`;
-    } else {
-      response = `END Merchant was not found!!`;
+  if (text.length > 0 && text.includes("*")) {
+    response = `CON validating inputs`;
+    let brokendown = text.split("*");
+    let merchantCode = brokendown[0];
+    let amount = brokendown[1];
+    response = `CON merchant input`;
+    if (Number(merchantCode) <= 0) {
+      response = `END you need to provide a correct merchant code format`;
     }
-  } else {
-    response = `END You didn't provide any input`;
+    response = `CON amoun input`;
+    if (Number(amount) <= 0) {
+      response = `END you need to provide the correct amount that is not less than or equal to 0`;
+    }
+    response = `CON Checking for existing merchant`;
+    if (Number(merchantCode) > 0 && Number(amount) > 0) {
+      response = `CON Looking for existing merchant`;
+      const existingMerchant = await merchantRecord.findOne({
+        merchantID: text,
+      });
+      if (existingMerchant) {
+        const newRecord = await new recordModel({
+          sessionId,
+          serviceCode,
+          phoneNumber,
+          text: amount,
+        });
+        response = `CON Saving request`;
+        await newRecord.save();
+        response = `END Completed`;
+      } else {
+        response = `END Merchant was not found!!`;
+      }
+    }
+  } else if (merchantRecord === "" || amount === "") {
+    response = `END You didn't provide any input (Merchant code together with Amount)`;
   }
 
   res.set("Content-Type: text/plain");
